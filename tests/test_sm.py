9da7d5fc4e89f613df64a5e0a7caa5e8b752a6b8
Jitendra wadle <jitendrawadle@gmail.com>
2017-09-25 08:33:20 +0530

    Update test_sm.py

--- tests/test_sm.py
+++ tests/test_sm.py
@@ -22,9 +22,9 @@ def test_verify_successfully_login(driver):
     obj.open_page_sm_home_page()
     obj.click_on_harburger_menu()
     obj.click_on_sign_link()
-    email = "test_@gmail.com"
-    username = "sm_@test"
-    password = "test@1234"
+    email = "xxxxxxx@xxxxxx"
+    username = "xxxxxx"
+    password = "xxxxxx"
     obj.fill_login_details(username=username, password=password)
     print "Pass"
     obj.click_on_sign_button()
